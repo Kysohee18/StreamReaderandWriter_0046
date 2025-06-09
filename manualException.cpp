fb7c498513e1909a06522ac6c5793d2a03de0cf6
Kyosohee18 <hanashahky@gmail.com>
2025-06-09 11:17:08 +0700

    menghapus file txt pada repository

--- manualException.cpp
+++ manualException.cpp
@@ -5,8 +5,8 @@ int main ()
 {
     try {
         cout << "selamat belajar di prodi ti umy " << endl;
-        throw 0.5; // melemparkan sebuah integer maka
-        cout << "pernyataan tidaka akan dieksekusi" << endl;
+       throw 0.5; // melemparkan sebuah integer maka
+        cout << "pernyataan tidak akan dieksekusi" << endl;
     }    
     catch ( int a) {
         // blok ini akan dieksekusi 
