e0ed7fcf18f96b8220bb5257bbfdcd84e959bfbc
Kyosohee18 <hanashahky@gmail.com>
2025-06-09 10:44:24 +0700

    program utama untuk memasukkan pesan array integer kedalam 3 elemen dan memanggil array

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -4,3 +4,15 @@
 #include <array>
 //untuk objek array yang digunakan
 using namespace std;
+
+
+int main() {
+    cout << "awal program " << endl; //penanda 1:...
+    try{
+        array<int,3> data ={1, 2, 3};
+        //pesan array integer 3 elemen
+        cout << data.at(5)<<endl;
+        //memanggil array elemen ke 5
+    }
+ 
+}
