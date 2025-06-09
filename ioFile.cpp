ead4d50fb0f607b5e90d68005ce6f5861bd240cf
Kyosohee18 <hanashahky@gmail.com>
2025-06-09 09:55:37 +0700

    operasi file ketika masuk ke dalam mode menulis

--- ioFile.cpp
+++ ioFile.cpp
@@ -4,3 +4,14 @@
 using namespace std;
 
 
+//StreamReaderandWriter_0046
+// minimal 10 commit
+
+int main () {
+    string baris;
+
+    //operasi file dalam mode menulis
+    ofstream outfile;
+    // menunjuk ke sebuah nama file untu membuka 
+
+}
